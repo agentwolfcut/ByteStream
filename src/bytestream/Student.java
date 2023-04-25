ae699c705c508e9d21b88fea21756f56a2661efa
Phurikorn <Phurikorn>
2023-04-25 18:37:52 +0700

    first commit

---
+++ src/bytestream/Student.java
@@ -0,0 +1,13 @@
+package bytestream;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    String id;
+    String name;
+
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
